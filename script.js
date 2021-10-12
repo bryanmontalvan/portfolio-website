00c66bcbceb181c9737d6e30e3821e010411221b
Bryan <bmontalvan100@gmail.com>
2021-10-11 20:55:45 -0400

    Working Draft of Version 1, fixed responsiveness on medium width screens

---
+++ script.js
@@ -0,0 +1,24 @@
+var imgArray = [
+	'imgs/technologies/c-logo.png',
+	'imgs/technologies/cpp-logo.png',
+	'imgs/technologies/java-logo.png',
+	'imgs/technologies/css3.png',
+	'imgs/technologies/javacript.png',
+	'imgs/technologies/html.png',
+	'imgs/technologies/git-logo.png',
+	'imgs/technologies/github-logo.png',
+	'imgs/technologies/linux.png',
+	'imgs/technologies/npmlogo.png',
+	'imgs/technologies/react-logo.png',
+	'imgs/technologies/sass-logo.png',
+	'imgs/technologies/tailwind-logo.png'
+]
+
+
+function showTechnologies(){
+	for (let i = 0; i < imgArray.length; i++){
+		
+	}
+}
+
+
